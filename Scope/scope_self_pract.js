ea42c8452ade658885acde43b7a86c42b8c1dbc9
vishalaher999 <vishalaher904@gmail.com>
2026-02-22 10:28:57 -0500

    Scope pract

---
+++ Scope/scope_self_pract.js
@@ -0,0 +1,17 @@
+{
+let a= 'Vishal';
+
+var c = 55;
+const b = 34;
+
+//console.log(a);
+//console.log(b);
+//console.log(c)
+}
+
+a= "Yes";
+b=101;
+c=201;
+console.log(a);
+console.log(b);
+console.log(c)
